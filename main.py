eccfb15f6bb69cbff57d8a67502291408e751d30
Xing-Huang <hxg@palliums.org>
2020-04-30 19:38:03 +0800

    v0.12

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ from libra.transaction import Script, TransactionPayload, SignedTransaction
 from json_rpc.views import EventView, TransactionView
 from test import create_accounts, create_client, create_accounts_with_coins
 
-[a1] = create_accounts(1)
-client = create_client()
-client.mint_coin(a1.address, 100, receiver_auth_key_prefix_opt=a1.auth_key_prefix, is_blocking=True)
-assert 100 == client.get_balance(a1.address)
+# [a1] = create_accounts(1)
+# client = create_client()
+# client.mint_coin(a1.address, 100, receiver_auth_key_prefix_opt=a1.auth_key_prefix, is_blocking=True)
+# assert 100 == client.get_balance(a1.address)
+
+# assert 0
+
