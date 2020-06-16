bd3c47147d49a8588e7ec4353f00d14cd99fa246
Xing-Huang <hxg@palliums.org>
2020-06-16 18:06:05 +0800

    change libra url

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from libra_client import Client, Wallet
 
 client = Client()
-wallet = Wallet.new()
-a1 = wallet.new_account()
-a2 = wallet.new_account()
-client.mint_coin(a1.address_hex, 1000, auth_key_prefix=a1.auth_key_prefix, currency_code="LBR")
-client.add_currency_to_account(a1, currency_code="Coin1")
-client.mint_coin(a1.address_hex, 1000, auth_key_prefix=a1.auth_key_prefix, currency_code="Coin1")
-print(client.get_account_state(a1.address_hex))
+# wallet = Wallet.new()
+# a1 = wallet.new_account()
+# a2 = wallet.new_account()
+# client.mint_coin(a1.address_hex, 1000, auth_key_prefix=a1.auth_key_prefix, currency_code="LBR")
+# client.add_currency_to_account(a1, currency_code="Coin1")
+# client.mint_coin(a1.address_hex, 1000, auth_key_prefix=a1.auth_key_prefix, currency_code="Coin1")
+# print(client.get_account_state(a1.address_hex))
