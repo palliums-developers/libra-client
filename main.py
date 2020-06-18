ca3c45c4e781d8fac8caddf7480913c08a119c04
Xing-Huang <hxg@palliums.org>
2020-06-18 16:34:14 +0800

    add gas_currency_code

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 from libra_client import Client, Wallet
 
+client = Client()
+wallet = Wallet.new()
+a1 = wallet.new_account()
+client.mint_coin(a1.address, 10000000, auth_key_prefix=a1.auth_key_prefix, currency_code="VLSUSD")
+client.add_currency_to_account(a1, "LBREUR", gas_currency_code="VLSUSD")
+client.mint_coin(a1.address, 10000000, auth_key_prefix=a1.auth_key_prefix, currency_code="LBREUR")
 
-client = Client.new("http://52.27.228.84:50001")
-print(client.get_currency_info("Coin1").get_to_lbr_exchange_rate())
-# print(client.get_transactions(0, 100))
-# wallet = Wallet.new()
-# a1 = wallet.new_account()
-# a2 = wallet.new_account()
-# client.mint_coin(a1.address_hex, 1000, auth_key_prefix=a1.auth_key_prefix, currency_code="LBR")
-# client.add_currency_to_account(a1, currency_code="Coin1")
-# client.mint_coin(a1.address_hex, 1000, auth_key_prefix=a1.auth_key_prefix, currency_code="Coin1")
-# print(client.get_balances(a1.address_hex))
