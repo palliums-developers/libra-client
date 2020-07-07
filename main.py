28c678961a05184e2253a9e5bbb296f994da7359
Xing-Huang <hxg@palliums.org>
2020-07-07 17:52:24 +0800

    chang expiration time

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ from lbrtypes.account_config import treasury_compliance_account_address, associa
 import time
 wallet = Wallet.new()
 client = Client()
-module_account = wallet.new_account()
-client.mint_coin(module_account.address, 10_000_000, auth_key_prefix=module_account.auth_key_prefix, is_blocking=True)
-client.swap_publish_contract(module_account)
-# client.set_exchange_module_address(module_account.address)
-print(client.get_sequence_number(module_account.address))
-
-client.swap_initialize(module_account, is_blocking=False)
-time.sleep(1)
-print(client.get_sequence_number(module_account.address))
+print(client.get_transaction(1).get_expiration_time())
+# module_account = wallet.new_account()
+# client.mint_coin(module_account.address, 10_000_000, auth_key_prefix=module_account.auth_key_prefix, is_blocking=True)
+# client.swap_publish_contract(module_account)
+# # client.set_exchange_module_address(module_account.address)
+# print(client.get_sequence_number(module_account.address))
+#
+# client.swap_initialize(module_account, is_blocking=False)
+# time.sleep(1)
+# print(client.get_sequence_number(module_account.address))
--- violas_client/json_rpc/views.py
+++ violas_client/json_rpc/views.py
@@ -467,7 +467,7 @@ class BlockMetadata(Struct):
     ]
 
     def get_timestamp_usecs(self):
-        return self.timestamp_usecs // 10**6
+        return self.timestamp_usecs
 
 class TransactionDataView(RustEnum):
     _enums = [
