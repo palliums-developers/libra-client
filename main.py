9b35881c008b7afc64e715e073d481823d66d374
Xing-Huang <hxg@palliums.org>
2020-07-07 18:37:58 +0800

    change ip

--- main.py
+++ main.py
@@ -3,15 +3,19 @@
 from exchange_client import Client, Wallet
 from lbrtypes.account_config import treasury_compliance_account_address, association_address
 import time
+from move_core_types.language_storage import core_code_address
+
+print(core_code_address().hex())
+
 wallet = Wallet.new()
-client = Client("violas_testnet")
-print(client.get_transaction(1))
-# module_account = wallet.new_account()
-# client.mint_coin(module_account.address, 10_000_000, auth_key_prefix=module_account.auth_key_prefix, is_blocking=True)
-# client.swap_publish_contract(module_account)
-# # client.set_exchange_module_address(module_account.address)
-# print(client.get_sequence_number(module_account.address))
-#
-# client.swap_initialize(module_account, is_blocking=False)
-# time.sleep(1)
-# print(client.get_sequence_number(module_account.address))
+client = Client()
+
+module_account = wallet.new_account()
+client.mint_coin(module_account.address, 10_000_000, auth_key_prefix=module_account.auth_key_prefix, is_blocking=True)
+client.swap_publish_contract(module_account)
+client.set_exchange_module_address(module_account.address)
+seq = client.swap_initialize(module_account)
+tx = client.get_account_transaction(module_account.address, seq)
+print(tx.get_code_type())
+# resource = client.get_account_state(module_account.address).swap_get_reserves_resource()
+# assert resource is not None
