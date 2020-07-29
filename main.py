21bb2e4e1648aded2d02ab3a8924a27069de054b
Xing-Huang <hxg@palliums.org>
2020-07-29 13:24:33 +0800

    change import

--- main.py
+++ main.py
@@ -4,10 +4,8 @@ from libra_client.wallet_library import Wallet
 # from lbrtypes.account_config import association_address, transaction_fee_address
 # from banktypes.bytecode import bytecodes, CodeType
 
-client = Client("libra_testnet")
+client = Client()
 wallet = Wallet.new()
 a1 = wallet.new_account()
 client.mint_coin(a1.address_hex, 22, auth_key_prefix=a1.auth_key_prefix, currency_code="Coin1")
-# balances = client.get_balances(a1.address_hex)
-# assert balances["LBR"] == 11
-# assert balances["Coin1"] == 22
+balances = client.get_balances(a1.address_hex)
