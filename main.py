646aa704358b5ad29690e901ffe6acc0f7f9bfbb
Xing-Huang <hxg@palliums.org>
2020-07-31 18:08:55 +0800

    change get data

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 from libra_client import Client as LibraClient
 from violas_client import Client as ViolasClient
 from violas_client import Wallet
+
+client = ViolasClient("violas_testnet")
+print(client.get_transaction(13827863).get_data())
