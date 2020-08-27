a4dc850ca204f5a8d5c66a25bd53d6c4e8b1dfc6
Xing-Huang <hxg@palliums.org>
2020-08-27 19:02:17 +0800

    change mint coin

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from libra_client import Wallet, Client
 
+client = Client()
 wallet = Wallet.new()
 a1 = wallet.new_account()
-client = Client()
-client.mint_coin(a1.address_hex, 11, auth_key_prefix=a1.auth_key_prefix, currency_code="LBR")
-client.add_currency_to_account(a1, "Coin1")
-client.mint_coin(a1.address_hex, 22, auth_key_prefix=a1.auth_key_prefix, currency_code="Coin1")
-balances = client.get_balances(a1.address_hex)
-assert balances["LBR"] == 11
-assert balances["Coin1"] == 22
+ac = client.get_account_state(a1.address)
+assert None == ac
+client.mint_coin(a1.address, 100, auth_key_prefix=a1.auth_key_prefix)
+print(client.get_balances(a1.address))
+# ac = client.get_account_state(a1.address)
+# assert None != ac
