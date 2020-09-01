a74e9b5880853c18a34bdf128a6226edb5f99186
Xing-Huang <hxg@palliums.org>
2020-09-01 14:55:00 +0800

    add uniswap

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
-from bank_client import Wallet, Client
-from move_core_types.language_storage import core_code_address
+from violas_client import Wallet, Client
+from violas_client.move_core_types.language_storage import core_code_address
 
 
-module_address = "da13aace1aa1c49e497416a9dd062ecb"
+bank_module_address = "da13aace1aa1c49e497416a9dd062ecb"
+exchange_module_address = "00000000000000000000000045584348"
 
-client = Client()
+client = Client("bj_testnet")
 client.set_bank_module_address(core_code_address())
-client.set_bank_owner_address(module_address)
+client.set_bank_owner_address(bank_module_address)
+client.set_exchange_module_address(core_code_address())
+client.set_exchange_owner_address(exchange_module_address)
 
-wallet = Wallet.new()
-a1 = wallet.new_account()
-client.mint_coin(a1.address, 300_000_000, auth_key_prefix=a1.auth_key_prefix, currency_code="USD")
-client.bank_publish(a1)
-client.bank_lock(a1, 200_000_000, currency_code="USD")
-client.bank_borrow(a1, 10_000_000, currency_code="USD")
-_, amount = client.bank_get_borrow_amount(a1.address, "USD")
+txs = client.get_transactions(23216000, 500)
 # client.bank_repay_borrow(a1, currency_code="USD", amount=amount)
 # assert client.bank_get_borrow_amount(a1.address, currency_code="USD")[0] == 0
