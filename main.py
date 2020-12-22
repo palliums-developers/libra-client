2f21f0c57dcf1b50c84c3c3394ac0c88fd3ba689
Xing-Huang <hxg@palliums.org>
2020-12-22 11:31:22 +0800

    change libra to diem

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from libra_client import Client, Wallet
 
 client = Client()
 wallet = Wallet.new()
-a1 = wallet.new_account()
-a2 = wallet.new_account()
+code = "charge vacuum raccoon demand smart jacket unit spare poverty hero ordinary ball pudding law puzzle party crucial accident aerobic dad diagram desert green spike"
+code = "cost oppose sense coyote cup spider hedgehog always catch total badge focus cable million fringe hello exhaust unfair dragon amazing local buzz noise ocean"
+v = Wallet.new_from_mnemonic(code)
+print(v.new_account().address_hex)
 
-print(client.get_currency_info("XUS"))
+
