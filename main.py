da6fa3bb2c82ce77045b61db6ecbbc1ae594eb12
Xing-Huang <hxg@palliums.org>
2021-05-12 10:30:20 +0800

    change uni

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ from libra_client import Client, Wallet
 cli = Client()
 w = Wallet.new()
 a1 = w.new_account()
-# cli.mint_coin(a1.address_hex, 22, auth_key_prefix=a1.auth_key_prefix, currency_code="VLS")
+cli.mint_coin(a1.address_hex, 22, auth_key_prefix=a1.auth_key_prefix, currency_code="VUSDT")
 # balances = cli.get_balances(a1.address_hex)
 # assert balances["VLS"] == 22
